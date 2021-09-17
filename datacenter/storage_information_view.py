ca78a17e29c39a42988c86392d0f960d16661157
asivolap <asivolap@openwaygroup.com>
2021-09-17 14:42:48 +0300

    fixed quotes

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -12,13 +12,13 @@ def storage_information_view(request):
         formatted_duration = format_duration(duration)
         is_strange = is_visit_long(duration)
         non_closed_visits = {
-                "who_entered": visit.passcard,
-                "entered_at": visit.entered_at,
-                "duration": formatted_duration,
-                "is_strange": is_strange,
+                'who_entered': visit.passcard,
+                'entered_at': visit.entered_at,
+                'duration': formatted_duration,
+                'is_strange': is_strange,
             }
         non_closed_list.append(non_closed_visits)
         context = {
-            "non_closed_visits": non_closed_list,
+            'non_closed_visits': non_closed_list,
         }
     return render(request, 'storage_information.html', context)
